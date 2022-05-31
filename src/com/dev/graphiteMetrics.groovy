0af150f87548b2406cbbeefe79fbc1fa3c9da0c2
bmanikantareddy <bmanikantareddy@gmail.com>
2022-05-31 20:59:33 +0530

    comments removal

--- src/com/dev/graphiteMetrics.groovy
+++ src/com/dev/graphiteMetrics.groovy
@@ -3,10 +3,6 @@ package com.dev
 
 
 class graphiteMetrics {
-    /**
-    * From a git URI, get the name of the repository
-    */
-    @NonCPS
     static String getepoch() {
         Date latestdate = new Date();
         return latestdate.getTime()
